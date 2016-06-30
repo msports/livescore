d69bd0057393983cff09300a38b93d5139c6094c
M SPORT <ego.itunes@icloud.com>
2016-07-01 02:37:57 +0630

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,24 +1,47 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
+import requests, bs4
 
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+res = requests.get('http://www.livescore.com/')
+
+res.raise_for_status()
+
+soup = bs4.BeautifulSoup(res.text,"html.parser")
+#team2 = str(".ply name")
+homeTeam = soup.findAll("div", attrs={"class":"ply tright name"})
+awayTeam = soup.findAll("div", attrs={"class" : "ply name"})
+score = soup.select('.scorelink')
+homeTeamNums = len(homeTeam)
+#teams = homeTeam + awayTeam
+if homeTeamNums != len(awayTeam) or homeTeamNums != len(score):
+	pass
+favTeam = [u'Barcelona', u'Real Madrid',u'Valencia',u'Atletico Madrid',u'Sevilla',u'Manchester United',
+		u'Chelsea',u'Liverpool',u'Arsenal',u'Manchester City',u'Leicester City',u'Tottenham Hotspur',
+		u'SSC Napoli',u'Juventus',u'Inter',u'Roma',u'AC Milan',u'Bayern Munich',
+		u'Borussia Dortmund',u'Paris Saint Germain']
+#minNum = min(len(score),10)
+#print(favTeam)
+#val  = 'Barcelona' in favTeam
+print("\n")
+for i in range(len(score)):
+	
+
+	hTeam = homeTeam[i].getText(strip = True)
+	#print(type(favTeam[6]))
+	#print(hTeam)
+	#print(type(str(hTeam)))
+	#print(type('Foysal'))
+	aTeam = awayTeam[i].getText(strip = True)
+	#print(type(aTeam))
+	#print(aTeam == u'Manchester United')
+	#print(str(hTeam) in favTeam)
+	#print(str(aTeam) in favTeam)
+	#print('Manchester United' in favTeam)
+	#val  = aTeam in favTeam
+	#print(val)
+	#print(str(aTeam) in favTeam)
+	#print(type(aTeam.encode('ascii','ignore')))#=='Manchester United')
+	if aTeam in favTeam or hTeam in favTeam:
+		print(hTeam.ljust(20,' ')+"\t"+score[i].getText()+'\t'+aTeam)
+		#print('hi')
+#print(awayTeam[1].getText())
+print('\n')
